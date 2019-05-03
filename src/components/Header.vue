ed5ffa951cefd818f0412d20791b85130b25a669
zenkronn-github <50049169+zenkronn-github@users.noreply.github.com>
2019-05-03 18:19:28 +0300

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,6 @@
       <button @click="openSidebar" :aria-label="$t('buttons.toggleSidebar')" :title="$t('buttons.toggleSidebar')" class="action">
         <i class="material-icons">menu</i>
       </button>
-      <img :src="logoURL" alt="File Browser">
       <search v-if="isLogged"></search>
     </div>
     <div>
